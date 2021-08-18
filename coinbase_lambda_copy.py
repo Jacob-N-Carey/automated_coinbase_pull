83e95288867a00c0e973637d35d5d9b96501ab68
Jacob Carey <jacobcarey@Jacobs-MBP.domain>
2021-08-18 08:18:51 -0600

    Added comments

--- coinbase_lambda_copy.py
+++ coinbase_lambda_copy.py
@@ -1,8 +1,10 @@
+# import libraries for making api requests and interacting with AWS
 import requests
 import json
 import boto3
 coinbase_uri = 'https://api.coindesk.com/v1/bpi/currentprice.json'
 
+# creates a connection to the AWS s3 bucket, calls the coinbase api, and loads the json from the request to s3
 def handler(event, context):
     AWS_BUCKET_NAME = 'bpi-price-bucket-00001'
     s3 = boto3.resource('s3')
--- main.tf
+++ main.tf
@@ -1,3 +1,4 @@
+# initializes terraform 
 terraform {
   required_providers {
     aws = {
@@ -7,15 +8,18 @@ terraform {
   }
 }
 
+# sets up aws as the provider we are going to be using
 provider "aws" {
   region = "us-east-1"
 }
 
+# creates the s3 bucket for us to load our json into 
 resource "aws_s3_bucket" "bpi-price-bucket" {
   bucket = "bpi-price-bucket-00001"
   acl           = "private"
 }
 
+# creates the iam policy for the coinbase lambda with the required permissions 
 resource "aws_iam_policy" "coinbase_lambda_iam_policy" {
   name        = "coinbase_lambda_iam_policy"
   path        = "/"
@@ -42,6 +46,7 @@ resource "aws_iam_policy" "coinbase_lambda_iam_policy" {
   })
 }
 
+# creates the iam role for the lambda 
 resource "aws_iam_role" "coinbase_lambda_iam_role" {
   name = "coinbase_lambda_iam_role"
 
@@ -62,17 +67,20 @@ resource "aws_iam_role" "coinbase_lambda_iam_role" {
 EOF
 }
 
+# attaches the iam role policy 
 resource "aws_iam_role_policy_attachment" "coinbase_lambda_role_policy_attach" {
   role       = aws_iam_role.coinbase_lambda_iam_role.name
   policy_arn = aws_iam_policy.coinbase_lambda_iam_policy.arn
 }
 
+# creates a zip file of the coinbase_lambda.py to load to lambda 
 data "archive_file" "default" {
   type        = "zip"
   source_dir  = "${path.module}/lambda/"
   output_path = "${path.module}/lambda_zip/lambda.zip"
 }
 
+# creates the lambda 
 resource "aws_lambda_function" "coinbase_lambda" {
   filename         = "${path.module}/lambda_zip/lambda.zip"
   function_name    = "coinbase_api_lambda_function"
@@ -83,17 +91,20 @@ resource "aws_lambda_function" "coinbase_lambda" {
   source_code_hash = filebase64sha256("${path.module}/lambda_zip/lambda.zip")
 }
 
+# sets up the cloudwatch rule to trigger the lambda every five minutes 
 resource "aws_cloudwatch_event_rule" "every_five_minutes" {
   name                = "every-five-minutes"
   description         = "Fires every five minutes"
   schedule_expression = "rate(5 minutes)"
 }
 
+# attaches the cloudwatch trigger to the lambda 
 resource "aws_cloudwatch_event_target" "call_coinbase_lambda_every_five_minutes" {
   rule = aws_cloudwatch_event_rule.every_five_minutes.name
   arn = aws_lambda_function.coinbase_lambda.arn
 }
 
+# allows cloudwatch and lambda to interact
 resource "aws_lambda_permission" "allow_cloudwatch_to_call_coinbase_lambda" {
   statement_id  = "AllowExecutionFromCloudWatch"
   action        = "lambda:InvokeFunction"
